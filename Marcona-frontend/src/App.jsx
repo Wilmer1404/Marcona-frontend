ac1e0b03ebd0afbe019d6f228c8c98962b4e9b2e
Wilmer1404 <samirloroamaya24@gmail.com>
2026-02-20 05:21:54 -0500

    archivos por default

---
+++ Marcona-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-marcona-500">
+      <h1 className="text-4xl font-bold text-white shadow-lg p-8 rounded-lg">
+        ¡SGD Marcona: Frontend con Tailwind funcionando!
+      </h1>
+    </div>
+  )
+}
+
+export default App
